49361f6ac9d374ec34c1b36d629b72ee5d897931
Brennan Fife <brenfife@gmail.com>
2020-06-13 16:23:00 -0500

    commit

--- controllers/book.go
+++ controllers/book.go
@@ -7,9 +7,76 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Don't need an ID property since it will automatically be generated
+type CreateBookInput struct {
+	Title  string `json:"title" binding:"required"`
+	Author string `json:"author" binding:"required"`
+}
+
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
 func FindBooks(c *gin.Context) {
 	var books []models.Book
 	models.DB.Find(&books)
 
 	c.JSON(http.StatusOK, gin.H{"data": books})
+}
+
+func CreateBook(c *gin.Context) {
+	// Validate input
+	var input CreateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Create book
+	book := models.Book{Title: input.Title, Author: input.Author}
+	models.DB.Create(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func FindBook(c *gin.Context) {
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func UpdateBook(c *gin.Context) {
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	models.DB.Model(&book).Updates(input)
+
+	c.JSON(http.StatusOK, gin.H{"data": book})
+}
+
+func DeleteBook(c *gin.Context) {
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
+
+	models.DB.Delete(&book)
+
+	c.JSON(http.StatusOK, gin.H{"data": true})
 }
--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func main() {
 	models.ConnectDatabase()
 
 	r.GET("/books", controllers.FindBooks)
-
+	r.GET("/books/:id", controllers.FindBook)
+	r.POST("/books", controllers.CreateBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
 	r.Run()
 }
